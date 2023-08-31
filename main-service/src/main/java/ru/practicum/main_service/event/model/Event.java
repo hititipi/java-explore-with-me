992fa4df0c0440baafc595058611fdb72be5962e
Ruslan Sabirov <r.sabirov@wisetech.pro>
2023-08-31 16:00:18 +0300

    fix: Query

--- main-service/src/main/java/ru/practicum/main_service/event/model/Event.java
+++ main-service/src/main/java/ru/practicum/main_service/event/model/Event.java
@@ -14,6 +14,7 @@ import static ru.practicum.main_service.utils.Constants.*;
 @Entity
 @Table(name = "events", schema = "public")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Event {
