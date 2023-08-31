fe5edbb3e39cebd71b5ed0a0e1396d023b5e65f8
Ruslan Sabirov <r.sabirov@wisetech.pro>
2023-08-31 16:26:09 +0300

    fix: Query

--- main-service/src/main/java/ru/practicum/main_service/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/main_service/event/repository/EventRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.main_service.event.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.main_service.event.model.Event;
 import ru.practicum.main_service.event.model.EventState;
@@ -21,7 +22,22 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findAllByCategoryId(long catId);
 
-    @Query("FROM Event AS e WHERE (:users IS NULL OR e.initiator.id IN (:users)) " +
+
+    @Query("select e from Event e " +
+            "where (coalesce(:userIds, null) is null or e.initiator.id in :userIds) " +
+            "and (coalesce(:states, null) is null or e.state in :states) " +
+            "and (coalesce(:categoryIds, null) is null or e.category.id in :categoryIds) " +
+            "and (coalesce(:rangeStart, null) is null or e.eventDate >= :rangeStart) " +
+            "and (coalesce(:rangeEnd, null) is null or e.eventDate <= :rangeEnd) ")
+    List<Event> findByAdmin(Pageable pageable,
+                            @Param("userIds") List<Long> userIds,
+                            @Param("states") List<EventState> states,
+                            @Param("categoryIds") List<Long> categoryIds,
+                            @Param("rangeStart") LocalDateTime rangeStart,
+                            @Param("rangeEnd") LocalDateTime rangeEnd
+                            );
+
+    /*@Query("FROM Event AS e WHERE (:users IS NULL OR e.initiator.id IN (:users)) " +
             "AND (:states IS NULL OR CAST(e.state AS string ) IN (:states)) " +
             "AND (:categories IS NULL OR e.category.id IN (:categories) ) " +
             "AND (:rangeStart IS NULL OR e.eventDate >= CAST(:rangeStart AS timestamp) ) " +
@@ -31,9 +47,30 @@ public interface EventRepository extends JpaRepository<Event, Long> {
                                List<EventState> states,
                                List<Long> categories,
                                LocalDateTime rangeStart,
-                               LocalDateTime rangeEnd);
+                               LocalDateTime rangeEnd);*/
 
-    @Query("FROM Event AS e WHERE (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
+    @Query("select e from Event e " +
+            "where e.state = 'PUBLISHED' " +
+            "and (coalesce(:text, null) is null or (lower(e.annotation) like lower(concat('%', :text, '%')) or lower(e.description) like lower(concat('%', :text, '%')))) " +
+            "and (coalesce(:categoryIds, null) is null or e.category.id in :categoryIds) " +
+            "and (coalesce(:paid, null) is null or e.paid = :paid) " +
+            "and e.eventDate >= :rangeStart " +
+            "and (coalesce(:rangeEnd, null) is null or e.eventDate <= :rangeEnd) " +
+            "and (:onlyAvailable = false or e.id in " +
+            "(select r.event.id " +
+            "from Request r " +
+            "where r.status = 'CONFIRMED' " +
+            "group by r.event.id " +
+            "having e.participantLimit - count(id) > 0 " +
+            "order by count(r.id))) ")
+    List<Event> findAllByPublic( Pageable pageable,
+                               @Param("text") String text,
+                               @Param("categoryIds") List<Long> categoryIds,
+                              @Param("paid") Boolean paid, @Param("rangeStart") LocalDateTime rangeStart,
+                              @Param("rangeEnd") LocalDateTime rangeEnd, @Param("onlyAvailable") Boolean onlyAvailable
+                             );
+
+   /* @Query("FROM Event AS e WHERE (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
             "AND (:categories IS NULL OR e.category.id IN (:categories)) " +
             "AND (:paid IS NULL OR e.paid IN :paid) " +
             "AND (:rangeStart IS NULL OR e.eventDate >= CAST(:rangeStart AS timestamp )) " +
@@ -47,5 +84,5 @@ public interface EventRepository extends JpaRepository<Event, Long> {
                                 Boolean paid,
                                 LocalDateTime rangeStart,
                                 LocalDateTime rangeEnd,
-                                Boolean onlyAvailable);
+                                Boolean onlyAvailable);*/
 }
