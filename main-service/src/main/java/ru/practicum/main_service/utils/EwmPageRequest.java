c35cf010db09bfc1a156acc0ba8118dfe5e6c81e
Ruslan Sabirov <r.sabirov@wisetech.pro>
2023-09-01 09:36:34 +0300

    feat: PageRequest

--- main-service/src/main/java/ru/practicum/main_service/utils/EwmPageRequest.java
+++ main-service/src/main/java/ru/practicum/main_service/utils/EwmPageRequest.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Sort;
 
 public class EwmPageRequest extends PageRequest {
 
-    public static EwmPageRequest of(int from, int size){
-       return new EwmPageRequest(from, size);
+    public static EwmPageRequest of(int from, int size) {
+        return new EwmPageRequest(from, size);
     }
 
     private EwmPageRequest(int from, int size) {
